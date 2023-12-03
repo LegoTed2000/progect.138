dbbb8bacb57f04e86eb0d5eb6ab6cd96c37513b1
LegoTed2000 <112373501+LegoTed2000@users.noreply.github.com>
2023-12-03 13:43:57 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -5,13 +5,22 @@ function setup() {
 	canvas = createCanvas(1240,336);
 	canvas.parent("canavs");
 
+	video = creatCapture(VIDEO);
+	video.hide
 
+	objectDetector = ml5.objectDetector("cocossd", modelLoaded)
 }
 
 function draw() {
-
+	image(video, 0, 0, 1240, 336);
 }
 
 function restart() {
     
+}
+
+function modelLoaded() {
+    console.log( "It worked");
+    
+    objectDetector.detect(img, gotResults);
 }
