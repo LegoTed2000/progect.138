7da2335db07b4f4e434a75eaec7606eb3e33ca55
LegoTed2000 <112373501+LegoTed2000@users.noreply.github.com>
2023-12-07 15:48:03 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -13,6 +13,21 @@ function setup() {
 
 function draw() {
 	image(video, 0, 0, 1240, 336);
+
+	if(scorerightwrist > 0.2) {
+        circle(rightWristx, rightWristy , 20);
+		
+		r = Math.random(255);
+		g = Math.random(255);
+		b = Math.random(255);
+
+		fill(r, g, b);
+		percent = floor(objects[i].confidence * 100)
+		noFill();
+		text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+		stroke(r, g, b);
+		rect(objects[i].x , objects[i].y, objects[i].width, objects[i].height);
+    }
 }
 
 function restart() {
@@ -23,4 +38,20 @@ function modelLoaded() {
     console.log( "It worked");
     
     objectDetector.detect(img, gotResults);
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+
+        scoreleftwrist = results[0].pose.keypoints[9].scoreleftwrist;
+
+        leftWristx = results[0].pose.leftWrist.x;
+        leftWristy = results[0].pose.leftWrist.y;
+        console.log("left wrist x is - "+ leftWristx + "left wrist y is - "+ leftWristy);
+
+        rightWristx = results[0].pose.rightWrist.x;
+        rightWristy = results[0].pose.rightWrist.xy;
+        console.log("right wrist x is - "+ rightWristx + "right wrist y is - "+ rightWristy);
+    }
 }
